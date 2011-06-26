73c4fa1827c9f58bc66b59ae8f8a84c5aceb5a76
Trey Doig <trey@seatme.com>
2011-06-26 09:56:50 -0700

    More sexiness in the UI

--- js/script.js
+++ js/script.js
@@ -44,4 +44,61 @@ $(function(){
 		      new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
 		  ), 13 // Zoom level
 		 );
+
+
+    var dataSet1 = new TimeSeries(), dataSet2 = new TimeSeries(), dataSet3 = new TimeSeries();
+ 
+    function mean() {
+	var m = (Math.round(Math.random() * 1000) / 100) + "";
+	if(m.length < 4) m = m + '0';
+	$("#chart-mean").html("$" + m);
+	return m;
+    }
+
+    function median() {
+	var m = (Math.round(Math.random() * 1000) / 100) + "";
+	if(m.length < 4) m = m + '0';
+	$("#chart-med").html("$" + m);
+	return m;
+    }
+    
+    setInterval(function() {
+        var now = new Date().getTime();
+        dataSet1.append(now, mean());
+        dataSet2.append(now, median());
+    }, 1000);
+    
+
+    $("#chart").attr('width', ($("#bottom").width() / 3)-60);
+    $(window).resize(function() {
+	$("#chart").attr('width', ($("#bottom").width() / 3)-60);
+    });
+    
+    // Build the timeline
+    var smoothie = new SmoothieChart({ 
+	millisPerPixel: 20, 
+	grid: { 
+	    strokeStyle: '#eeeeee', 
+	    fillStyle: '#ffffff', 
+	    lineWidth: 1, 
+	    millisPerLine: 1000, 
+	    verticalSections: 10
+	}
+    });
+    
+    smoothie.addTimeSeries(dataSet1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 1 });
+    smoothie.addTimeSeries(dataSet2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 1 });
+    
+    smoothie.streamTo(document.getElementById('chart'), 1000);
+    
+
+    $("#hide-legend").click(function() {
+	if($(this).html() == '(hide)') {
+	    $("#legend div").hide();
+	    $(this).html('(show)');
+	} else { 
+	    $("#legend div").show();
+	    $(this).html('(hide)');
+	}
+    });
 });
