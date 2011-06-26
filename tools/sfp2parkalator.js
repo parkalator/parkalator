cb06c47ef9cbd8c8988e5323d64efcab15f041c8
Vishal Verma <v@slowpoison.net>
2011-06-25 22:08:14 -0700

    sfp2parkalator node.js commit

--- tools/sfp2parkalator.js
+++ tools/sfp2parkalator.js
@@ -2,33 +2,57 @@ var sys = require('sys');
 var fs = require('fs');
 
 
-var sfp2parkalator = function(err, contents) {
-    if (err) throw err;
-    var count = 0;
+function sfp2parkalator(contents, time) {
 
     var o = JSON.parse(contents);
     if (o == null)
         throw "Couldn't parse file";
+    var l = 0;
+    var myrates = new Array();
+
+    var mins = time2mins(time);
+    //console.log("Mins is " + mins);
 
     var avls = o.AVL;
     for (i=0; i < avls.length; ++i) {
         if (typeof avls[i].BFID !== 'undefined') {
             var rates = avls[i].RATES.RS;
             for (j=0; j < rates.length; ++j) {
+                rates[j].BEGMIN = timestrToMin(rates[j].BEG, 0);
+                rates[j].ENDMIN = timestrToMin(rates[j].END, 1);
+
+                if (rates[j].BEGMIN > mins ||
+                    rates[j].ENDMIN < mins)
+                    continue;
+
                 rates[j].ID = avls[i].BFID;
                 rates[j].NAME = avls[i].NAME;
                 rates[j].OCC = avls[i].OCC;
                 rates[j].OPER = avls[i].OPER;
                 rates[j].LOC = avls[i].LOC;
 
-                rates[j].BEGMIN = timestrToMin(rates[j].BEG, 0);
-                rates[j].ENDMIN = timestrToMin(rates[j].END, 1);
-                console.log(rates[j]);
+                myrates[l++] = rates[j];
             }
         }
     }
+
+    return myrates;
 }
 
+function time2mins(ts) {
+    //var d = Date.parse(ts) / 1000;
+    //return (d - (parseInt(d / 86400)*86400)) / (24*60); 
+    var d = ts.match(/\w+ \w+ \d+ (\d+):(\d+)/);
+    //console.log(d[1] + " and " + d[2]);
+    return parseInt(d[1]) * 60 + parseInt(d[2]);
+}
+
+
+var doMunge  = function(err, contents) {
+    if (err) throw err;
+    var d = new Date();
+    console.log(sfp2parkalator(contents, d.toLocaleString()));
+}
 
 function timestrToMin(timestr, end)
 {
@@ -56,5 +80,5 @@ function timestrToMin(timestr, end)
 }
 
 
-fs.readFile(process.argv[2], sfp2parkalator);
+fs.readFile(process.argv[2], doMunge);
 //sys.puts(process.argv[1]);
