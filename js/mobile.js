7c65d5779765d004a4f935cb67836f595f76bea4
Zac Bowling <zac@zacbowling.com>
2011-08-06 18:17:28 -0700

    mobile

--- js/mobile.js
+++ js/mobile.js
@@ -120,7 +120,7 @@ function loadLines (map,toolbar)
 				//map_overlays.push(marker);
 				map_overlays.push(line);
 			}
-			toolbar.setTitle("Full/Empty/Total:&nbsp;" + occCount + "/" + emptyCount + "/" + totalCount);
+			toolbar.setTitle("Occupied: " + occCount + " - Empty: " + emptyCount);
 			
 		}
 		setTimeout(function(){loadLines(map,toolbar);},3000);
